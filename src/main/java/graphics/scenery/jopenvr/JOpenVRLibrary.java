ae82101cf72847a4f446c7e643d78c53ecc8e6aa
Ulrik Guenther <hello@ulrik.is>
2017-01-28 20:57:42 +0100

    Updated version strings

--- src/main/java/graphics/scenery/jopenvr/JOpenVRLibrary.java
+++ src/main/java/graphics/scenery/jopenvr/JOpenVRLibrary.java
@@ -784,18 +784,17 @@ public class JOpenVRLibrary implements Library {
 	/** Original signature : <code>char* VR_GetVRInitErrorAsEnglishDescription(EVRInitError)</code> */
 	public static native Pointer VR_GetVRInitErrorAsEnglishDescription(int error);
 
-	public static String IVRSystem_Version = "FnTable:IVRSystem_012";
-	public static String IVRApplications_Version = "FnTable:IVRApplications_006";
-	public static String IVRSettings_Version = "FnTable:IVRSettings_001";
+	public static String IVRSystem_Version = "FnTable:IVRSystem_015";
 	public static String IVRExtendedDisplay_Version = "FnTable:IVRExtendedDisplay_001";
+	public static String IVRTrackedCamera_Version = "FnTable:IVRTrackedCamera_003";
+	public static String IVRApplications_Version = "FnTable:IVRApplications_006";
 	public static String IVRChaperone_Version = "FnTable:IVRChaperone_003";
 	public static String IVRChaperoneSetup_Version = "FnTable:IVRChaperoneSetup_005";
-	public static String IVRCompositor_Version = "FnTable:IVRCompositor_016";
-	public static String IVROverlay_Version = "FnTable:IVROverlay_013";
+	public static String IVRCompositor_Version = "FnTable:IVRCompositor_019";
+	public static String IVROverlay_Version = "FnTable:IVROverlay_014";
 	public static String IVRRenderModels_Version = "FnTable:IVRRenderModels_005";
-	public static String IVRControlPanel_Version = "FnTable:IVRControlPanel_001";
 	public static String IVRNotifications_Version = "FnTable:IVRNotifications_002";
-	public static String IVRTrackedCamera_Version = "FnTable:IVRTrackedCamera_003";
+	public static String IVRSettings_Version = "FnTable:IVRSettings_002";
 	public static String IVRScreenshots_Version = "FnTable:IVRScreenshots_001";
 	public static String IVRResources_Version = "FnTable:IVRResources_001";
 }
